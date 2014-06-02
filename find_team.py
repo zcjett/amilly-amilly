c41d2ecb4e5d27ee33c151934e31fc68b46bfbf9
Chris Poirel <chris@redowlanalytics.com>
2014-06-01 20:52:47 -0400

    working version with random teams

--- find_team.py
+++ find_team.py
@@ -3,20 +3,23 @@
 import argparse
 from player_salary_scores import PlayerSalaryScores
 from knapsack import ModifiedKnapsack
+from mcmc import TeamMCMC
 
 CAPACITY = 35000
-RESTRICTIONS = {'P': 1,
-                'C': 1,
-                '1B': 1,
-                '2B': 1,
-                'SS': 1,
-                '3B': 1,
-                'OF': 3}
+TEAM_COMP = {'P': 1,
+             'C': 1,
+             '1B': 1,
+             '2B': 1,
+             'SS': 1,
+             '3B': 1,
+             'OF': 3}
 
 def main():
     parser = argparse.ArgumentParser(description='Find dat team.')
     parser.add_argument('salaries', help='File containing player salaries and positions.')
     parser.add_argument('projections', help='File containing projected points.')
+    parser.add_argument('--knapsack', help='Find a team using the modified knapsack approach.')
+    parser.add_argument('--mcmc', help='Find a team using the MCMC approach.')
     args = parser.parse_args()
 
     players = PlayerSalaryScores()
@@ -27,9 +30,12 @@ def main():
     classes = [players.get_position(n) for n in names]
     values = [players.get_score(n) for n in names]
     weights = [players.get_salary(n) for n in names]
-    knapsack = ModifiedKnapsack(names, classes, values, weights, CAPACITY, RESTRICTIONS)
 
-    knapsack.find_solution()
+    knapsack = ModifiedKnapsack(names, classes, values, weights, CAPACITY, TEAM_COMP)
+#    knapsack.find_solution()
+
+    mcmc = TeamMCMC(names, classes, values, weights, CAPACITY, TEAM_COMP)
+    team = mcmc.find_solution()
 
 if __name__ == '__main__':
     main()
