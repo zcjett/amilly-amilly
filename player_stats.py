78701ca556ff1b2f176e3ea997f67151ee95bfdd
Chris Poirel <chris@redowlanalytics.com>
2014-06-08 16:59:04 -0400

    more stats!

--- player_stats.py
+++ player_stats.py
@@ -13,7 +13,9 @@ class PlayerStats:
         # self.read_pitcher_home_away()
         # self.read_pitcher_left_right()
         # self.read_pitcher_total_stats()
-        self.read_catcher_stats()
+        # self.read_catcher_stats()
+        # self.read_batter_left_right()
+        self.read_batter_total_stats()
 
         self.printStats()
 
@@ -46,7 +48,7 @@ class PlayerStats:
 
 
     def read_pitcher_total_stats(self):
-        stats = ['gs', 'k', 'ip']
+        stats = ['gs_total', 'k_pitched_total', 'ip_total']
         years = [2013, 2014]
         for year in years:
             infile = '%s/Pitcher/%d/%d Total Pitcher Stats.csv' %(self.statsDir, year, year)
@@ -59,7 +61,7 @@ class PlayerStats:
 
 
     def read_catcher_stats(self):
-        stats = ['sb_allowed', 'cs']
+        stats = ['sb_catcher', 'cs_catcher']
         years = [2013, 2014]
         for year in years:
             infile = '%s/Catcher/%d Catcher Stats.csv' %(self.statsDir, year)
@@ -68,6 +70,46 @@ class PlayerStats:
             for items in reader:
                 player = items[0].lower()
                 for i, stat_val in enumerate([float(x) for x in items[2:4]]):
+                    self.stats[player][stats[i]][year] = stat_val
+
+    def read_batter_left_right(self):
+        stats = ['pa', 'hr', 'k', 'woba']
+        years = [2013, 2014]
+        for year in years:
+            for hand in ['RHP', 'LHP']:
+                infile = '%s/Batter/%d/%d Batter Stats vs %s.csv' %(self.statsDir, year, year, hand)
+                reader = csv.reader(open(infile), quotechar='"')
+                header = reader.next()
+                for items in reader:
+                    player = items[0].lower()
+                    for i, stat_val in enumerate([float(x) for x in items[2:6]]):
+                        self.stats[player][stats[i]][year][hand] = stat_val
+
+
+    def read_batter_total_stats(self):
+        stats = ['1b_total',
+                 '2b_total',
+                 '3b_total',
+                 'h_total',
+                 'bb_total',
+                 'bb_percentage_total',
+                 'hr_total',
+                 'ab_total',
+                 'pa_total',
+                 'ba_total',
+                 'g_total',
+                 'sb_total',
+                 'cs_total']
+        years = [2013, 2014]
+        for year in years:
+            infile = '%s/Batter/%d/%d Total Batter Stats.csv' %(self.statsDir, year, year)
+            reader = csv.reader(open(infile), quotechar='"')
+            header = reader.next()
+            for items in reader:
+                player = items[0].lower()
+                for i, stat_val in enumerate([float(x.rstrip('%')) for x in items[2:15]]):
+                    if stats[i]=='bb_percentage_total':
+                        stat_val/=100.0
                     self.stats[player][stats[i]][year] = stat_val
 
 
