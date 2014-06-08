823a80be5e8afc7b04acd7c29e8580945c590e76
Chris Poirel <chris@redowlanalytics.com>
2014-06-08 17:33:08 -0400

    Adding ballpark stats

---
+++ ballpark_stats.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+import csv
+import json
+
+class BallparkStats:
+
+    def __init__(self, statsDir):
+        self.statsDir = statsDir.rstrip('/')
+
+        self.stats = defaultdict(dict)
+
+        self.read_ballpark_stats()
+        self.printStats()
+
+
+    def read_ballpark_stats(self):
+        stats = ['overall', 'avg_lhb', 'avg_rhb', 'hr_lhb', 'hr_rhb']
+        infile = '%s/Park Factor/Ball Park Factor.csv' %(self.statsDir)
+        print infile
+        reader = csv.reader(open(infile), quotechar='"', )
+        header = reader.next()
+        print header
+        for items in reader:
+            team = items[0].upper()
+            for i, stat_val in enumerate([float(x) for x in items[1:6]]):
+                self.stats[team][stats[i]] = stat_val
+
+
+    def printStats(self):
+        print json.dumps(self.stats, indent=4)
--- find_team.py
+++ find_team.py
@@ -6,6 +6,7 @@ from player_salary_scores import PlayerSalaryScores
 from knapsack import ModifiedKnapsack
 from mcmc import TeamMCMC
 from player_stats import PlayerStats
+from ballpark_stats import BallparkStats
 
 CAPACITY = 35000
 TEAM_COMP = {'P': 1,
@@ -20,29 +21,31 @@ def main():
     parser = argparse.ArgumentParser(description='Find dat team.')
     parser.add_argument('salaries', help='File containing player salaries and positions.')
     parser.add_argument('projections', help='File containing projected points.')
-    parser.add_argument('stats', help='Directory containing player stats.')
+    parser.add_argument('stats', help='Directory containing all stats.')
     parser.add_argument('--knapsack', help='Find a team using the modified knapsack approach.')
     parser.add_argument('--mcmc', action='store_true', help='Find a team using the MCMC approach.')
     args = parser.parse_args()
 
-    players = PlayerSalaryScores()
-    players.read_positions_and_salaries(args.salaries)
-    players.read_projections(args.projections)
+    # players = PlayerSalaryScores()
+    # players.read_positions_and_salaries(args.salaries)
+    # players.read_projections(args.projections)
+    #
+    # names = players.get_names()
+    # classes = [players.get_position(n) for n in names]
+    # values = [players.get_score(n) for n in names]
+    # weights = [players.get_salary(n) for n in names]
+    #
+    # if args.knapsack:
+    #     knapsack = ModifiedKnapsack(names, classes, values, weights, CAPACITY, TEAM_COMP)
+    #    #knapsack.find_solution()
+    #
+    # if args.mcmc:
+    #     mcmc = TeamMCMC(names, classes, values, weights, CAPACITY, TEAM_COMP)
+    #     mcmc.find_simulated_annealing_solution()
+    #
+    # player_stats = PlayerStats(args.stats)
 
-    names = players.get_names()
-    classes = [players.get_position(n) for n in names]
-    values = [players.get_score(n) for n in names]
-    weights = [players.get_salary(n) for n in names]
-
-    if args.knapsack:
-        knapsack = ModifiedKnapsack(names, classes, values, weights, CAPACITY, TEAM_COMP)
-       #knapsack.find_solution()
-
-    if args.mcmc:
-        mcmc = TeamMCMC(names, classes, values, weights, CAPACITY, TEAM_COMP)
-        mcmc.find_simulated_annealing_solution()
-
-    stats = PlayerStats(args.stats)
+    ballpark_stats = BallparkStats(args.stats)
 
 
 
